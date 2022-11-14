03459ae26c445922dccc725c04d6a5c6719291c6
aldyrozian <aldyshimozikato@gmail.com>
2022-11-15 00:24:21 +0700

    first commit

---
+++ resources/views/koordinator/koordinator.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts/main')
+@section('container')
+</main>
+<h1 style="text-align:center;">Sistem Tugas Akhir 2</h1>
+
+        <div class="d-flex justify-content-center">
+            <a class="btn my-3" href="koordinator/list-pendaftaran-ta-1" role="button"
+                style="background-color:#ff8c1a; width: 20rem;">Pendaftaran Administrasi TA 2</a>
+        </div>
+
+
+        <div class="d-flex justify-content-center">
+            <a class="btn my-3" href="/koordinator/hasil-review-proposal" role="button"
+                style="background-color:#ff8c1a; width: 20rem;">Hasil Review
+                Penelitian</a>
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <a class="btn my-3" href="/koordinator/list-pendaftaran-seminar-ta-1" role="button"
+                style="background-color:#ff8c1a; width: 20rem;">Pendaftaran
+                Seminar TA 2</a>
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <a class="btn my-3" href="/koordinator/jadwal-seminar" role="button"
+                style="background-color:#ff8c1a; width: 20rem;">Jadwal
+                Seminar</a>
+        </div>
+        <div class="d-flex justify-content-center">
+            <a class="btn my-3" href="/koordinator/penilaian-seminar" role="button"
+                style="background-color:#ff8c1a; width: 20rem;">Penilaian
+                Seminar</a>
+        </div>
+
+
+
+@endsection
