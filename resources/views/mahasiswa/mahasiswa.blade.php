7069457e63fb71ccb8ce51e2e38405d9825ee0bd
aldyrozian <aldyshimozikato@gmail.com>
2022-11-17 19:17:41 +0700

    Ddelt

--- resources/views/mahasiswa/mahasiswa.blade.php
+++ resources/views/mahasiswa/mahasiswa.blade.php
@@ -29,11 +29,5 @@
         style="background-color:#ff8c1a; width: 20rem;">Pendaftaran
         Seminar TA 2</a>
 </div>
-<div class="d-flex justify-content-center">
-    <a class="btn my-3
-    @if(!isset(auth()->user()->penilaianSeminar))
-    disabled
-    @endif
-    " href="/mahasiswa/revisi-seminar" role="button" style="background-color:#ff8c1a; width: 20rem;">Revisi Seminar</a>
-</div>
+
 @endsection
